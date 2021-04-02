898b6b0e1329de261e84eb98fa8ab6c660896df8
qiaobuyun <qbyun130@163.com>
2021-04-02 16:19:27 +0800

    import

--- src/main/java/com/qby/kafka/CustomProducer.java
+++ src/main/java/com/qby/kafka/CustomProducer.java
@@ -2,6 +2,7 @@ package com.qby.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
@@ -14,16 +15,16 @@ public class CustomProducer {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties props = new Properties();
-//kafka 集群，broker-list
-        props.put("bootstrap.servers", "172.16.13.168:9092");
+        //kafka 集群，broker-list
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.16.13.168:9092");
         props.put("acks", "all");
-//重试次数
+        //重试次数
         props.put("retries", 1);
-//批次大小
+        //批次大小 16k
         props.put("batch.size", 16384);
-//等待时间
+        //等待时间
         props.put("linger.ms", 1);
-//RecordAccumulator 缓冲区大小
+        //RecordAccumulator 缓冲区大小
         props.put("buffer.memory", 33554432);
         props.put("key.serializer",
                 "org.apache.kafka.common.serialization.StringSerializer");
@@ -31,7 +32,7 @@ public class CustomProducer {
                 "org.apache.kafka.common.serialization.StringSerializer");
         Producer<String, String> producer = new
                 KafkaProducer<>(props);
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10; i++) {
             producer.send(new ProducerRecord<String, String>("test03",
                     Integer.toString(i), Integer.toString(i)));
         }
