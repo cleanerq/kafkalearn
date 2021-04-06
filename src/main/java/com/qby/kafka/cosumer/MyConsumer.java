2ee875a3c560c9d992105092b0b4f81e03a0cb84
qiaobuyun <qbyun130@163.com>
2021-04-06 15:10:50 +0800

    import

--- src/main/java/com/qby/kafka/cosumer/MyConsumer.java
+++ src/main/java/com/qby/kafka/cosumer/MyConsumer.java
@@ -1,9 +1,12 @@
 package com.qby.kafka.cosumer;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -12,8 +15,8 @@ public class MyConsumer {
         Properties props = new Properties();
         props.put("bootstrap.servers", "172.16.13.168:9092");
         props.put("group.id", "test");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put("key.deserializer",
                 "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer",
@@ -21,12 +24,13 @@ public class MyConsumer {
         KafkaConsumer<String, String> consumer = new
                 KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList("test03"));
+
         while (true) {
             ConsumerRecords<String, String> records =
-                    consumer.poll(100);
+                    consumer.poll(Duration.of(100, ChronoUnit.MILLIS));
             for (ConsumerRecord<String, String> record : records)
-                System.out.printf("offset = %d, key = %s, value = %s%n",
-                        record.offset(), record.key(), record.value());
+                System.out.printf("offset = %d, key = %s, value = %s, partition = %d %n",
+                        record.offset(), record.key(), record.value(), record.partition());
         }
     }
 }
